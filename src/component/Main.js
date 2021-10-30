23c930529d27a7fe90b52d7f654e5316f9f6cd02
LATIsi <p.kmj9911@gmail.com>
2021-10-30 18:54:04 +0900

    bg_2

---
+++ src/component/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../style/Background_style.css';
+
+function Main({ children }) {
+  return <Main className="main_ground">{children}</Main>;
+}
+
+export default Main;
